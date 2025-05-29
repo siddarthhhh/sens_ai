1829b5d36a809c2ee6dbd405481e927c21e74245
siddarthhhh <chidurulasiddarth@gmail.com>
2025-05-29 17:25:48 +0530

    solved clerk import error using useEffect and Fetch

--- components/header.jsx
+++ components/header.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useEffect } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import {
@@ -24,12 +24,15 @@ import {
   StarIcon,
 } from "lucide-react";
 
-const Header = async() => {
-  // await checkUser();
+const Header = () => {
+
+  useEffect(() => {
+    fetch("/api/check-user");
+  }, []);
+
   return (
     <header className="fixed top-0 z-50 w-full border-b bg-background/80 backdrop-blur-md supports-[backdrop-filter]:bg-background">
       <nav className="container mx-auto flex h-16 items-center justify-between px-4">
-        {/* Logo */}
         <Link href="/">
           <Image
             src="/logo.png"
@@ -40,9 +43,7 @@ const Header = async() => {
           />
         </Link>
 
-        {/* Right side */}
         <div className="flex items-center space-x-2 md:space-x-4">
-          {/* Dashboard button */}
           <SignedIn>
             <Link href="/dashboard">
               <Button variant="outline">
@@ -50,39 +51,38 @@ const Header = async() => {
                 <span className="hidden md:block">Industry Insights</span>
               </Button>
             </Link>
-          
 
-          {/* Growth Tools Dropdown */}
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <Button>
-                <StarIcon className="h-4 w-4" />
-                <span className="hidden md:block">Growth Tools</span>
-                <ChevronDown className="h-4 w-4" />
-              </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent>
-               <DropdownMenuItem>
-              <Link href="/resume"  className="flex item-center gap-2">
-                  <FileText className="h-4 w-4 mr-2" />
-                  <span>build resume</span>
-               </Link>   
-               </DropdownMenuItem>
-                
-              <DropdownMenuItem>
-                <Link href="/ai-cover-letter"  className="flex item-center gap-2">
-                  <PenBox className="h-4 w-4 mr-2" />
-                  <span>Cover Letter</span>
-               </Link>  </DropdownMenuItem>
-              <DropdownMenuItem><Link href="/interview"  className="flex item-center gap-2">
-                  <GraduationCap className="h-4 w-4 mr-2" />
-                  <span>Interview Prep</span>
-               </Link>  </DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button>
+                  <StarIcon className="h-4 w-4" />
+                  <span className="hidden md:block">Growth Tools</span>
+                  <ChevronDown className="h-4 w-4" />
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent>
+                <DropdownMenuItem>
+                  <Link href="/resume" className="flex item-center gap-2">
+                    <FileText className="h-4 w-4 mr-2" />
+                    <span>Build Resume</span>
+                  </Link>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <Link href="/ai-cover-letter" className="flex item-center gap-2">
+                    <PenBox className="h-4 w-4 mr-2" />
+                    <span>Cover Letter</span>
+                  </Link>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <Link href="/interview" className="flex item-center gap-2">
+                    <GraduationCap className="h-4 w-4 mr-2" />
+                    <span>Interview Prep</span>
+                  </Link>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
           </SignedIn>
 
-          {/* Auth buttons */}
           <SignedOut>
             <SignInButton>
               <Button variant={"outline"}>
@@ -93,10 +93,10 @@ const Header = async() => {
           <SignedIn>
             <UserButton
               appearance={{
-                elements:{
-                  avatarBox:'w-10 h-10',
-                  userButtonPopoverCard:"shadow-xl",
-                  userPreviewMainIdentifier:"front-semibold"
+                elements: {
+                  avatarBox: 'w-10 h-10',
+                  userButtonPopoverCard: "shadow-xl",
+                  userPreviewMainIdentifier: "front-semibold"
                 }
               }}
               afterSignOutUrl="/"
